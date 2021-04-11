973f3a8d311705f844f1792d3489374e8c79f4f5
hatef <rahmani.hatef@gmail.com>
2021-04-11 18:49:59 +0000

    cmd: add unit test

---
+++ cmd/command_test.go
@@ -0,0 +1,35 @@
+package cmd
+
+import "testing"
+
+func addOne(val uint16) uint16 {
+	return val + 1
+}
+
+func power(val uint16) uint16 {
+	return val * val
+}
+
+func TestCommand(t *testing.T) {
+	one := NewCommand("c", Value, addOne)
+	if !one.IsValue() {
+		t.Error("the type of command should be value")
+	}
+
+	if one.Exe(2) != 3 {
+		t.Error("the execution of func addOne is invalid")
+	}
+
+	if one.IsPointer() {
+		t.Error("the type of command should not be value")
+	}
+
+	power := NewCommand("c", Pointer, power)
+	if !power.IsPointer() {
+		t.Error("the type of command should be pointer")
+	}
+
+	if power.Exe(2) != 4 {
+		t.Error("the execution of func power is invalid")
+	}
+}
