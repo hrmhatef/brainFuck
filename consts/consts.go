8a06a5959bc88931e579569247d1c8c2e1f44b22
hatef <rahmani.hatef@gmail.com>
2021-04-12 11:37:28 +0000

    const: add some new consts and edit an error message

--- consts/consts.go
+++ consts/consts.go
@@ -8,5 +8,16 @@ var (
 	DuplicateCommand = errors.New("the command is already exist")
 	InvalidMemory    = errors.New("Index of memory id out of range")
 	InvalidOperator  = errors.New("Unknown operator")
-	InvalidSymbol    = errors.New("unable to delete this symbol")
+	InvalidSymbol    = errors.New("unable to delete or add this symbol")
+)
+
+const (
+	Plus  rune = '+'
+	Dash  rune = '-'
+	Dot   rune = '.'
+	Comma rune = ','
+	Left  rune = '<'
+	Right rune = '>'
+	Start rune = '['
+	End   rune = ']'
 )
